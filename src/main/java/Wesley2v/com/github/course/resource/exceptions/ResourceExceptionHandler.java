2cc3cbf38edf2426ada4cf263866b2be4e2b757c
Wesley Sousa <wesleyltsousa@gmail.com>
2025-10-03 10:09:56 -0300

    Exception handling - update

--- src/main/java/Wesley2v/com/github/course/resource/exceptions/ResourceExceptionHandler.java
+++ src/main/java/Wesley2v/com/github/course/resource/exceptions/ResourceExceptionHandler.java
@@ -13,6 +13,14 @@ import java.time.Instant;
 @ControllerAdvice
 public class ResourceExceptionHandler {
 
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+        String error = "Resource not found";
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
     @ExceptionHandler(DatabaseException.class)
     public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request) {
         String error = "Database error";
@@ -21,10 +29,4 @@ public class ResourceExceptionHandler {
         return ResponseEntity.status(status).body(err);
     }
 
-    public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
-        String error = "Resource not found";
-        HttpStatus status = HttpStatus.NOT_FOUND;
-        StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
-        return ResponseEntity.status(status).body(err);
-    }
 }
--- src/main/java/Wesley2v/com/github/course/services/exceptions/ResourceNotFoundException.java
+++ src/main/java/Wesley2v/com/github/course/services/exceptions/ResourceNotFoundException.java
@@ -3,6 +3,6 @@ package Wesley2v.com.github.course.services.exceptions;
 public class ResourceNotFoundException extends RuntimeException{
 
     public ResourceNotFoundException(Object id) {
-        super("Resource not foud. Id " + id);
+        super("Resource not found. Id " + id);
     }
 }
